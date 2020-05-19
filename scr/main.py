75c591078fbc1e352ee01f5b32ef0c2a28f4860d
mattgonley <mattgonley@gmail.com>
2020-05-19 13:39:25 -0400

    inital set up for program

---
+++ scr/main.py
@@ -0,0 +1,10 @@
+"""
+:author: Matt Gonley
+:date: May 19, 2020
+:description: Main file for taking playlist url, and sending it to be parsed.
+"""
+
+
+def main()
+    pLink = input("enter Playlist url: ")
+    print("Your link was: " + pLink)
